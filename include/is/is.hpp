5ca83f9c4e7c6b9d2df5e6051433a86249fe3463
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2017-11-20 19:38:47 -0200

    added json io helpers

--- include/is/is.hpp
+++ include/is/is.hpp
@@ -16,6 +16,7 @@
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include <fstream>
 
 namespace is {
 
@@ -391,6 +392,27 @@ po::variables_map parse_program_options(int argc, char** argv,
 
   if (vm.count("help")) print_help();
   return vm;
+}
+
+inline void save_to_json(std::string const& filename, pb::Message const& message) {
+  pb::JsonPrintOptions options;
+  options.add_whitespace = true;
+  options.always_print_primitive_fields = true;
+  std::string json;
+
+  pb::MessageToJsonString(message, &json, options);
+
+  std::ofstream file(filename);
+  file << json;
+  file.close();
+}
+
+template <typename T>
+inline boost::optional<T> load_from_json(std::string const& filename) {
+  T message;
+  std::ifstream in(filename);
+  std::string buffer((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+  return is::pb::JsonStringToMessage(buffer, &message).ok() ? boost::optional<T>(message) : boost::none;
 }
 
 }  // namespace is
